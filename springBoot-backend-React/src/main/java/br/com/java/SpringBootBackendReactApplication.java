5472aff2cab8fa8ff76396a7218e94a28dd91799
Diegotpereira <diegoteixeirapereira@hotmail.com>
2021-04-30 17:28:55 -0300

    criação dos arquivos classes java do projeto.

---
+++ springBoot-backend-React/src/main/java/br/com/java/SpringBootBackendReactApplication.java
@@ -0,0 +1,13 @@
+package br.com.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBackendReactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootBackendReactApplication.class, args);
+	}
+
+}
